cac6e12caf84ed85c97bf536e0fc5df429423052
NadirBakhsh <nadir4k2010@gmail.com>
2022-12-26 00:28:19 +0500

    booleanAndIntegersValues

---
+++ booleanAndIntegersValues.sol
@@ -0,0 +1,16 @@
+// Booleans
+// bool: The possible values are constants true and false.
+bool bool isBool = true;
+
+
+// Integers
+// There is two type of Integers uint / int these value type contain bit format, 8 up-to 256
+// Uint mean unsigned number value like
+uint public number1;
+uint8 public number2;
+uint256 public number3;
+
+// int means signed number value
+int public number1 = 24;
+int public number2 = 230;
+int public number3 = 200;
