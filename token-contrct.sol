7dab190560c291ac8cf953d1364f9ed582547ff0
NadirBakhsh <nadir4k2010@gmail.com>
2023-07-22 22:31:54 +0500

    line brake

--- token-contrct.sol
+++ token-contrct.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Hn_erc20_jan is ERC20 {
+
     constructor(uint256 initialsupply) ERC20("Hn_erc20_jan", "AKS"){
         _mint(msg.sender, initialsupply);
     }
+    
 }
